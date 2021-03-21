d3f32b472072c373b1cb5724f9eec07815cd4d12
Kamrujjaman Joy <joykamrujjaman59@gmail.com>
2021-03-21 15:35:08 +0600

    day18

--- lib/pages/home_detail_page.dart
+++ lib/pages/home_detail_page.dart
@@ -15,15 +15,54 @@ class DetailsPage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(),
         backgroundColor: MyTheme.creamColor,
+        bottomNavigationBar: Container(
+          color: Colors.white,
+          child: ButtonBar(
+            alignment: MainAxisAlignment.spaceBetween,
+            buttonPadding: EdgeInsets.zero,
+            children: [
+              "\$ ${catalog.price}".text.bold.xl4.red800.make(),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(MyTheme.darkBlueshColor),
+                  shape: MaterialStateProperty.all(
+                    StadiumBorder(),
+                  ),
+                ),
+                child: "Buy".text.make(),
+              ).wh(100, 40),
+            ],
+          ).p16(),
+        ),
         body: Column(
           children: [
             Hero(
               tag: Key(catalog.id.toString()),
               child: Image.network(catalog.image),
-            ),
+            ).h32(context),
             Expanded(
-              child: Column(
-                children: [],
+              child: VxArc(
+                height: 30.0,
+                arcType: VxArcType.CONVEY,
+                edge: VxEdge.TOP,
+                child: Container(
+                  color: Colors.white,
+                  width: context.screenWidth,
+                  child: Column(
+                    children: [
+                      catalog.name.text.bold.xl4
+                          .color(MyTheme.darkBlueshColor)
+                          .make(),
+                      catalog.description.text
+                          .textStyle(context.captionStyle)
+                          .xl
+                          .make(),
+                      10.heightBox,
+                    ],
+                  ).py64(),
+                ),
               ),
             ),
           ],
